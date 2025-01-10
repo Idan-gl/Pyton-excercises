268a1f670e036ecd1dd3efd7685bd8ef37847b14
Idan Gamliel <IDANGAMLIEL38@GMAIL.COM>
2025-01-10 15:23:50 +0200

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[21]:
-
-
 def idan (num):
     lista = []
     
     while len(lista)<numb:
         num = int(input())
         lista = lista + [num]
+        
     for x in lista:
         if x > num:
-                num = x
+            num = x
     return (num)
+    
 numb = int(input("how many"))
 idan(numb)
-
-
-# In[ ]:
-
-
-
-
