136d17de9dad5ff3ad1739051397fb008a5b01a3
Idan Gamliel <IDANGAMLIEL38@GMAIL.COM>
2025-01-11 12:30:41 +0200

    Update 11.py

--- 11.py
+++ 11.py
@@ -7,6 +7,7 @@ for i in shalom:
         a[i]=b
     print(a)
 
+
 shalom = {'avi' : 4, 'is' : 3, 'best' : 7, 'moshe' : 3, 'dudi' : 4}
 shalom1 = {'avi' : 4, 'is' : 3, 'good' : 7, 'moshe' : 3, 'tzipi' : 4}
 for i in shalom:
