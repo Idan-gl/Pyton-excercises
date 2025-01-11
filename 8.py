c63647e5d2aa7245c04513ccfaf9b5c602c6a527
Idan Gamliel <IDANGAMLIEL38@GMAIL.COM>
2025-01-11 12:33:20 +0200

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,23 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[10]:
-
-
 a=open('idan.txt','a')
 a.write('\n Python is sababa')
 a.close()
 
 
-# In[13]:
-
-
 a=open('idan.txt','r')
 a.read()
-
-
-# In[ ]:
-
-
-
-
